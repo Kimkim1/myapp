c57bb32e01b13c330fef60c450a213002ea9852b
Kimkim1 <Julykimkim05@gmail.com>
2018-07-29 16:12:06 +0800

    Heloo, Kim

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-	render html:"hello, Kim!"
+	render html:"hello, Kim! how are you"
   end
 end
